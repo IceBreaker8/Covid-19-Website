69d31d9d708e5872b017757d705c7ae51a006089
Ryder05 <mohamedoussema.mhiri@ensi-uma.tn>
2020-08-27 17:53:01 +0100

    zoom

--- covid19/static/js/script.js
+++ covid19/static/js/script.js
@@ -71,7 +71,7 @@
                  {
                      var circle = L.circle([lat, long], {
                          color: "#ff416c",
-                         fillColor: '#f03',
+                         fillColor: "#f03",
                          fillOpacity: 0.4,
                          radius: radius,
                          color: "#ff416c",
@@ -86,7 +86,7 @@
                      console.log("green called");
                      var circle = L.circle([lat, long], {
                          color: "#00FA9A",
-                         fillColor: '#00FA9A',
+                         fillColor: "#00FA9A",
                          fillOpacity: 0.6,
                          radius: radius,
                          color: "#00FA9A",
@@ -101,7 +101,7 @@
                  {
                      var circle = L.circle([lat, long], {
                          color: "#9370DB",
-                         fillColor: '#9370DB',
+                         fillColor: "#9370DB",
                          fillOpacity: 0.6,
                          radius: radius,
                          color: "#9370DB",
@@ -136,56 +136,71 @@
 
  function mff() {
      cont.innerHTML = "";
-     for (var i = 0; i < json_obj.length; i++) {
+     for (let i = 0; i < json_obj.length; i++) {
+         let j=0;
          let flag_url = json_obj[i]["countryInfo"]["flag"];
          let cases = json_obj[i]["cases"]
          let name = json_obj[i].country
          document.getElementsByClassName("map-sidebar-sort")[0].innerHTML = "Total Cases";
-         cont.innerHTML += `<div class="map-sidebar-section-item">
+         cont.innerHTML += `<div class="map-sidebar-section-item" id=`+i+`>
          <div class="map-sidebar-section-item-img"><img class="flag-img" src=` + flag_url + `></div>
         <div class="map-sidebar-section-item-details">
           <div class="map-sidebar-section-item-title" id="label">` + name + `</div>
     </div>
      <div class="map-sidebar-section-item-nb infected">` + cases + `</div>
     </div> `;
+    // $(".map-sidebar-section-content").on("click",$(".map-sidebar-section-item"),function(){
+    //     console.log(i);
+    //    })
      }
+     for (let i = 0; i < json_obj.length; i++) {
+        $("#"+i).on("click",function(){
+            let lat = json_obj[this.id]["countryInfo"]["lat"];
+            console.log("lat");
+            let long = json_obj[this.id]["countryInfo"]["long"];
+            mymap.setView([lat,long]);
+        })}
  }
 
  function mf(jsonfile, SortBy) {
      cont.innerHTML = "";
+     var str_;
+     var state;
      switch (SortBy) {
          case "TC":
-             var str_ = "cases";
-             var state = "infected";
+             str_ = "cases";
+             state = "infected";
              break;
          case "CPM":
-             var str_ = "casesPerOneMillion";
+             str_ = "casesPerOneMillion";
+             state = "infected";
+
              break;
          case "Rec":
-             var str_ = "recovered";
-             var state = "recovered";
+             str_ = "recovered";
+             state = "recovered";
              break;
          case "Ds":
-             var str_ = "deaths";
-             var state = "dead";
+             str_ = "deaths";
+             state = "dead";
              break;
          case "FR":
-             var str_ = "cases";
-             var state = "recovered";
+             str_ = "cases";
+             state = "recovered";
              break;
          case "DPM":
-             var str_ = "deathsPerOneMillion";
-             var state = "dead";
+             str_ = "deathsPerOneMillion";
+             state = "dead";
              break;
          case "Act":
-             var str_ = "sick";
+             str_ = "sick";
              break;
          case "name":
-             var str_ = "cases";
-             var state = "infected";
+             str_ = "cases";
+             state = "infected";
              break;
          default:
-             var str_ = "cases";
+             str_ = "cases";
 
      }
      for (var i = 0; i < jsonfile.length; i++) {
@@ -193,16 +208,28 @@
          let name = jsonfile[i].country
          let Report = jsonfile[i][str_]
          console.log(state);
-         cont.innerHTML += `<div class="map-sidebar-section-item">
+         cont.innerHTML += `<div class="map-sidebar-section-item" id=`+i+` >
          <div class="map-sidebar-section-item-img"><img class="flag-img" src=` + flag_url + `></div>
         <div class="map-sidebar-section-item-details">
           <div class="map-sidebar-section-item-title">` + name + `</div>
     </div>
      <div class="map-sidebar-section-item-nb `.concat(state) + `">` + Report + `</div>
     </div> `;
-     }
- }
+    //$(".map-sidebar-section-item").on("click",function(){console.log("Bitch yes please");})
 
+ }
+ console.log(jsonfile);
+ for (let i = 0; i < jsonfile.length; i++) {
+    $("#"+i).on("click",function(){
+        let lat = jsonfile[this.id]["countryInfo"]["lat"];
+        console.log(lat);
+        let long = jsonfile[this.id]["countryInfo"]["long"];
+        mymap.setView([lat,long]);
+    })}
+
+ //$(".map-sidebar-section-item").on("click",function(){console.log("Bitch yes please");})
+ //$(".map-sidebar-section-content").on("click",$("div.map-sidebar-section-item"),function(){console.log("Bitch noo please");})
+ }
  function numberWithCommas(x) {
      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
  }
