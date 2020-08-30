f6afa5902269fd2a161904059e6b82a81475d0a7
ahmadfrikha2016@gmail.com <59843500+IceBreaker8@users.noreply.github.com>
2020-08-30 22:02:18 +0100

    -Added a slash

--- covid19/urls.py
+++ covid19/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('overview/', views.overview, name="covid19-overview"),
     path('map/overview/', views.overview, name="covid19-map-overview"),
     path('map/', views.map, name="covid19-map"),
-    path('map/statistics/<slug:country_id>', views.stats, name="covid19-stats"),
+    path('map/statistics/<slug:country_id>/', views.stats, name="covid19-stats"),
     path('subscription/', views.subs, name="covid19-subs"),
     path('about/', views.about, name="covid19-about"),
 
